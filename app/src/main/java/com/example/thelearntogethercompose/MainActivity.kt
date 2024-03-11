4f81f53870db877e03b6d6b81303d735dbed41ba
rami <khamadekh@gmail.com>
2024-03-11 17:02:05 +0300

    task completed

--- app/src/main/java/com/example/thelearntogethercompose/MainActivity.kt
+++ app/src/main/java/com/example/thelearntogethercompose/MainActivity.kt
@@ -3,13 +3,23 @@ package com.example.thelearntogethercompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.sourceInformationMarkerEnd
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.thelearntogethercompose.ui.theme.TheLearnTogetherComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ArticleCardApp()
                 }
             }
         }
@@ -30,17 +40,48 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+private fun ArticleCardApp () {
+    ArticleCard(
+        image = painterResource(id = R.drawable.bg_compose_background),
+        title = stringResource(id = R.string.title_compose_tutorial),
+        shortDescription = stringResource(id = R.string.compose_short_desc),
+        longDescription = stringResource(id = R.string.compose_long_desc)
     )
+
+}
+
+@Composable
+private fun ArticleCard(
+    image: Painter,
+    title: String,
+    shortDescription: String,
+    longDescription: String,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier) {
+        Image(painter = image, contentDescription = null)
+        Text(
+            text = title,
+            modifier = Modifier.padding(16.dp),
+            fontSize = 24.sp
+        )
+        Text(
+            text = shortDescription,
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+        Text(
+            text = longDescription,
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Justify
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     TheLearnTogetherComposeTheme {
-        Greeting("Android")
+        ArticleCardApp()
     }
 }
